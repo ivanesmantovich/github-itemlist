0bcaad11dc4cf89f1c5d42ee4410ce12279ccbdd
Ivan Esmantovich <ivanesmantovich@gmail.com>
2021-10-06 11:39:03 +0300

    Deleted comments

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,6 @@ import relativeTime from 'dayjs/plugin/relativeTime';
 dayjs.extend(relativeTime);
 import './App.css';
 
-// PROP TIPS
-// Next, let’s figure out what data each component needs to render. This will give us the props and propTypes for each one.
-// See if you can figure out the propTypes definition for each of these without looking ahead at the code. The FileList should take one prop, files, which is an array of file objects.
-// The FileListItem will just take a single file object as the file prop. That object should have a name, type, a commit with a message, and a last-modified time.
-// FileName will take a file object and expect it to have a name property.
-// FileIcon will take a file object and use its type property to decide which kind of icon to show.
-// CommitMessage will take a commit object and expect it to have a message property.
-// Finally, Time will take an absolute time string. We’re going to reuse the Time component we made for Tweet, propTypes and all.
-// PROP TIPS
-
 interface TimeProps {
 	time: string; // Например '1999-01-01' / '2021-09-13 20:09:09'
 }
